02698da56414af5964e3e1ed02cf87d2569cc21c
junwoo45 <sinyoorang@gmail.com>
2020-05-13 23:40:59 +0900

    feat: main 함수 실행 확인

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// main function is the starting point for Flutter Apps!
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello Jun'),
+      ),
+    ),
+  );
+}
